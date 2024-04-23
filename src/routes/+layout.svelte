aec6da4fb6bf8ef3712f87dfaf401757c9aee61b
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-04-24 00:34:53 +0700

    Add `favicon` and improve page title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script>
   import "../app.pcss"
   import { initializeStores, Modal } from '@skeletonlabs/skeleton'
+  import { dev } from '$app/environment'
 
   initializeStores()
 </script>
 
+<svelte:head>
+  <title>Shakerr {dev ? 'Dev' : ''}</title>
+</svelte:head>
 <Modal />
 <slot></slot>
